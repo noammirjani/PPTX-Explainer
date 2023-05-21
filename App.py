7e18cb33b1cd311a38328fe5015aef724a59a27c
Noam Mirjani <Noamirjani@gmail.com>
2023-05-21 19:02:15 +0300

    working connection with gpt-api

--- App.py
+++ App.py
@@ -41,11 +41,38 @@ def extract_text_from_presentation(file_path):
     return prs_content
 
 
+def ai_text_analyzer(presentation):
+    import openai
+    import time
+
+    OPENAI_API_KEY = "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"
+
+    openai.api_key = OPENAI_API_KEY
+    system_prompt = "You're an AI text analyzer assisting with presentation summarization.For each slide's content you"\
+                    " receive, generate a concise summary of the text.\n User:'Slide content'\nAI:'Summary explanation'"
+    messages = [{"role": "system", "content": system_prompt}]  # the behavior of the system
+    responses = []
+
+    for index, slide_content in enumerate(presentation):
+        # set instructions to the chat as a user message (the slide content)
+        messages.append({"role": "user", "content": slide_content})
+        # sends the request
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+        # get response
+        chat_response = completion.choices[0].message.content
+        # keeping the history of the chat
+        messages.append({"role": "assistant", "content": chat_response})
+        responses.append(chat_response + "\n")
+        time.sleep(60)
+
+    return responses
+
+
 def main():
-    print("-"*20, "Hello ChatBot", "-"*20)
+    print("-" * 20, "Hello ChatBot", "-" * 20)
     file_path = get_file_path()
     presentation_content = extract_text_from_presentation(file_path)
-    print(presentation_content)
+    print(ai_text_analyzer(presentation_content))
 
 
 if __name__ == "__main__":
